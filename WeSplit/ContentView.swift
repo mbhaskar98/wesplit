5155d60311ed86eb7ee4490b8e11d737274cbe8a
Bhaskar Mahajan <bmahajan@zscaler.com>
2024-06-03 01:44:14 +0530

    init

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Bhaskar Mahajan on 02/06/24.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
